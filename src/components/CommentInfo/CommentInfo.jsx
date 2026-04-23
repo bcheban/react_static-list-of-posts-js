2a1011a8917d8ee772bf759c2df07d077b22488e
bcheban <128683111+bcheban@users.noreply.github.com>
2026-04-23 19:22:46 +0300

    solution

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -10,7 +10,6 @@ export const CommentInfo = ({ comment }) => (
         <a className="CommentInfo__email" href={`mailto:${comment.email}`}>
           {comment.email}
         </a>
-        
       </div>
 
       <div className="CommentInfo__body">{comment.body}</div>
